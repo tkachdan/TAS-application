5324ad68790be4349b20ceed910befb1c8e21e5a
Krasotin <fixlet@gmail.com>
2014-12-11 20:12:00 +0100

    Merge fix

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,16 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import src.persistence.models.Poi;
 import src.service.PoiService;
 import src.service.impl.PoiServiceImpl;
-import views.html.add;
-import views.html.edit;
-import views.html.index;
-import views.html.indexLogined;
+import views.html.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Application extends Controller {
 
@@ -18,6 +18,7 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    //TODO: can be put id to checkbox for future proceeding
     public static Result renderIndexLogined() {
         String str = "";
         if (session().isEmpty()) {
@@ -25,15 +26,31 @@ public class Application extends Controller {
         } else {
             PoiService poiService = new PoiServiceImpl();
             List<Poi> poiList = poiService.getAllPois();
+            int checkboxId = 0;
             for (Poi p : poiList) {
                 str += "<tr>";
-                str += " <td> <input type=\"checkbox\" name=\"myTextEditBox\" value=\"checked\" /> </td>";
+                str += " <td> <input type=\"checkbox\" id=" + checkboxId + " name=\"" + checkboxId + "\" value=\"checked\" /> </td>";
                 str += "<td>" + p.getId() + "</td> <td>" + p.getName() + "</td> <td>" + p.getType() + "</td> <td>"
                         + p.getCost() + "</td>";
                 str += "</tr>";
+                checkboxId++;
             }
             return ok(indexLogined.render(str));
         }
+    }
+
+    public static Result renderTrip() {
+        Map<String, String> data = Form.form().bindFromRequest().data();
+        List<Poi> pois = new ArrayList<>();
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+
+        }
+
+        /*String str = Form.form().bindFromRequest().get("0");
+        System.out.println("DATA:" + str);*/
+        return ok(trip.render("trip"));
     }
 
     public static Result renderAdd() {
@@ -45,52 +62,5 @@ public class Application extends Controller {
     }
 
 
-
-/*    public static Result addBar() {
-        Bar bar = Form.form(Bar.class).bindFromRequest().get();
-        bar.save();
-        //System.out.println("NOOO");
-        return redirect(routes.Application.index());
-    }
-
-    public static Result getBars() {
-        //first param String.class -> primary key
-        List<Bar> bars = new Model.Finder(String.class, Bar.class).all();
-        java.util.Collections.sort(bars);
-        String str = "";
-
-        for (Bar b : bars) {
-            str += "<tr>";
-            str += "<td>" + b.id + "</td> <td>" + b.name + "</td>";
-            str += "</tr>";
-
-        }
-        return ok(viewAll.render(str));
-
-
-    }
-
-    public static Result deleteAll(){
-        List<Bar> bars = new Model.Finder(String.class, Bar.class).all();
-        for(Bar b : bars) {
-            b.delete();
-        }
-
-        return redirect(routes.Application.index());
-    }
-
-    public static Result editBar() {
-        System.out.println("YEE");
-        Bar bar = Form.form(Bar.class).bindFromRequest().get();
-
-        System.out.println(bar.id + " " + bar.name);
-
-        Bar.update(bar.id, bar);
-
-
-        return redirect(routes.Application.index());
-
-
-    }*/
 
 }
